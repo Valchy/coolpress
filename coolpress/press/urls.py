6e528648cb901d3ea0c61bc4bf507c18f34e5758
Valchy <valchygaming@gmail.com>
2021-10-22 15:42:50 +0200

    -hf

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [ # Question: why exactly do we specify a name attribute?
+urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:post_id>', views.post_detail, name='posts-detail'),
     path('posts/', views.post_list, name='posts-list'),
