c010d8dff8bc8f196035450ce392b42a63abdbce
Valeri Sabev <valchygaming@gmail.com>
2021-10-22 02:53:15 +0200

    BIG UPDATE

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [ # Question: why exactly do we specify a name attribute?
     path('', views.index, name='index'),
     path('post/<int:post_id>', views.post_detail, name='posts-detail'),
-    path('posts/', views.post_list, name='posts-list')
+    path('posts/', views.post_list, name='posts-list'),
+    path('categories', views.category_list, name='category-list'),
+    path('category/<str:category_slug>', views.category_posts, name='category-posts'),
 ]
 
 # Handling error pages
