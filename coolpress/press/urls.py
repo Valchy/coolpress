b6637d52c44b21a0c46a845860c39f708a0bc216
Valchy <valchygaming@gmail.com>
2021-10-26 15:10:43 +0200

    Bug fix

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # Home and About Page view
     path('', views.index, name='index'),
-    path('about', views.About, name='about-page'),
+    path('about/', views.About.as_view(), name='about-page'),
 
     # Post views
     path('posts/', views.post_list, name='posts-list'), # All posts
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/update/<int:post_id>/', views.post_update, name='post-update'), # Update a post
 
     # Category views
-    path('categories', views.category_list, name='category-list'), # All categories
+    path('categories/', views.category_list, name='category-list'), # All categories
     path('category/<str:category_slug>/', views.category_posts, name='category-posts') # Specific category
 ]
 
