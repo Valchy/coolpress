16051ef5735c1bb4a859430960b0286a93bf72b1
Valchy <valchygaming@gmail.com>
2021-10-26 20:24:20 +0200

    Tests are now working

--- coolpress/press/tests.py
+++ coolpress/press/tests.py
@@ -2,11 +2,11 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
-from press.models import Category, CoolUser
+from press.models import CoolUser
 
 
 class PostModelTest(TestCase):
 	def test_sample_post(self):
 		u = User.objects.create(first_name='juantigo')
-		cu = CoolUser.objects.create(u)
+		cu = CoolUser.objects.create(user=u)
 		self.assertEqual(cu.id, 1)
